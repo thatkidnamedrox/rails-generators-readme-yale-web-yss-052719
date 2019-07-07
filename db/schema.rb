e4168184003f5c1f7299c5055a7f99b2b63a360b
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-07-07 23:40:02 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190707220201) do
+ActiveRecord::Schema.define(version: 20190707233951) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20190707220201) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.string   "post_status"
   end
 
 end
